02b5b98746a22b2a9acdb9a574cb0f50e7292edc
antoine_marchal <antoine.marchal@obspm.fr>
2016-01-30 18:12:18 +0100

    patch in funtion

--- find_SZE.py
+++ find_SZE.py
@@ -13,36 +13,15 @@ filename_smooth = "maps_smooth/HFI_SkyMap_857_2048_R2.00_full.fits"
 map_smooth,header = hp.read_map(filename_smooth,h=True)
 #hp.mollview(map_smooth, norm='hist')
 
-patch_size = 2000 #FIXME
+patch_size = 100 #FIXME
 """------------------------------------------------------------
    -------Reading galactic coordonate into .fits file ---------
    ------------------------------------------------------------"""
 PSZ = "PSZ2v1.fits"
 GLON,GLAT = fct.coord_sz(PSZ)
+#w.wcs.crval = [121.1743 ,-21.5733]
 
-"""------------------------------------------------------------
-   -------------Creation of wcs objects for patch--------------
-   ------------------------------------------------------------"""
-w = wcs.WCS(naxis=2)
-w.wcs.crpix = [patch_size/2, patch_size/2] #FIXME +1 si 0 ou 1
-#w.wcs.crval = [GLON[0],GLAT[0]]
-w.wcs.crval = [121.1743 ,-21.5733]
-n_side      = hp.get_nside(map_smooth)
-pix_size    = ma.sqrt(hp.nside2pixarea(n_side, degrees=True))
-w.wcs.cdelt = np.array([-pix_size/2., pix_size/2.])
-w.wcs.ctype = ["GLON-TAN", "GLAT-TAN"]               # use a gnomonic projetion
-
-header = w.to_header() #FIXME
-
-patch   = np.zeros((patch_size, patch_size))
-new_map = patch
-xx, yy  = np.indices((patch_size, patch_size))
-
-patch_GLON, patch_GLAT = w.wcs_pix2world(xx, yy, 0)
-patch_PHI = patch_GLON * ma.pi / 180. 
-patch_THETA = patch_GLAT * ma.pi / 180.
-patch = hp.ang2pix(n_side,ma.pi / 2. - patch_THETA, patch_PHI)
-new_map = map_smooth[patch]
+new_map = fct.patch_map(map_smooth, patch_size, GLON, GLAT)
 
 plt.imshow(new_map)
 
--- function.py
+++ function.py
@@ -1,13 +1,21 @@
+from astropy.io import fits as pyfits
+from astropy import wcs 
+import numpy as np
+import healpy as hp
+import matplotlib.pyplot as plt
+import math as ma
+import os
+
+plt.ion()
+
 def smooth(filename, fwhm) :
-	import healpy as hp
-	import numpy as np
 	map,header = hp.read_map(filename,h=True)
 	# map_size = hp.get_map_size(map)
 	#hp.mollview(map, norm='hist')
 	# print(hp.npix2nside(map_size))
-
-        print fwhm
-	fwhm_rad = np.radians(fwhm/60.)
+        
+    print fwhm
+    fwhm_rad = np.radians(fwhm/60.)
 	print fwhm_rad
 	map_gauss = hp.smoothing(map, fwhm_rad)
 	#hp.mollview(map_gauss, norm='hist')
@@ -17,9 +25,35 @@ def smooth(filename, fwhm) :
 def coord_sz(filename) :
         import astropy.table as ta
         import astropy.io.fits as af
-
+        
         cat = af.getdata(filename)
         data = ta.Table(cat)
         RA = data['RA']
         DEC = data['DEC']
         return (RA,DEC)
+    
+def patch_map(map_smooth, patch_size, GLON, GLAT) :
+    """------------------------------------------------------------
+   -------------Creation of wcs objects for patch--------------
+   ------------------------------------------------------------"""
+    w = wcs.WCS(naxis=2)
+    w.wcs.crpix = [patch_size/2, patch_size/2] #FIXME +1 si 0 ou 1
+    w.wcs.crval = [GLON[0],GLAT[0]]
+    n_side      = hp.get_nside(map_smooth)
+    pix_size    = ma.sqrt(hp.nside2pixarea(n_side, degrees=True))
+    w.wcs.cdelt = np.array([-pix_size/2., pix_size/2.])
+    w.wcs.ctype = ["GLON-TAN", "GLAT-TAN"]     # use a gnomonic projetion
+    
+    header = w.to_header() #FIXME
+    
+    patch   = np.zeros((patch_size, patch_size))
+    new_map = patch
+    xx, yy  = np.indices((patch_size, patch_size))
+    
+    patch_GLON, patch_GLAT = w.wcs_pix2world(xx, yy, 0)
+    patch_PHI = patch_GLON * ma.pi / 180. 
+    patch_THETA = patch_GLAT * ma.pi / 180.
+    patch = hp.ang2pix(n_side,ma.pi / 2. - patch_THETA, patch_PHI)
+    new_map = map_smooth[patch]
+
+    return new_map
