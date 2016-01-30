87b382154eaecf91ea50ebfb5121faa400dc75c7
antoine_marchal <antoine.marchal@obspm.fr>
2016-01-30 18:34:50 +0100

    creation of structure new_map

--- find_SZE.py
+++ find_SZE.py
@@ -9,21 +9,20 @@ import os
 import function as fct
 plt.ion()
 
-filename_smooth = "maps_smooth/HFI_SkyMap_857_2048_R2.00_full.fits"
-map_smooth,header = hp.read_map(filename_smooth,h=True)
-#hp.mollview(map_smooth, norm='hist')
-
 patch_size = 100 #FIXME
-"""------------------------------------------------------------
-   -------Reading galactic coordonate into .fits file ---------
-   ------------------------------------------------------------"""
 PSZ = "PSZ2v1.fits"
 GLON,GLAT = fct.coord_sz(PSZ)
 #w.wcs.crval = [121.1743 ,-21.5733]
 
-new_map = fct.patch_map(map_smooth, patch_size, GLON, GLAT, 0)
+new_map = [] # déclare une liste
 
-plt.imshow(new_map)
+unit_1 = open("filenames_HFI.txt")
+for i, line in enumerate(unit_1) :
+    filename_smooth = line.strip()
+    map_smooth,header = hp.read_map(filename_smooth,h=True)
+    new_map[i] = fct.patch_map(map_smooth, patch_size, GLON, GLAT, 0)
+
+plt.imshow(new_map[0])
 
 
 
