88f6576299f09a039bad43f13e34362e5cd454bc
antoine_marchal <antoine.marchal@obspm.fr>
2016-01-31 17:48:13 +0100

    fix

--- find_SZE.py
+++ find_SZE.py
@@ -22,7 +22,7 @@ path_1 = "maps_smooth/"
 for line in unit_1:
     filename_smooth = line.strip()
     map_smooth,header = hp.read_map(path_1 + filename_smooth[10:],h=True)
-   patch_map.append(
+    patch_map.append(
         (filename_smooth[21:24], fct.patch_map(map_smooth, patch_size, GLON[0], GLAT[0]))
         )
 #plt.imshow(patch_map[0][1]) 
