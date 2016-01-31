3d9d6a24f04f6fec6bf674809607c0477e06c008
antoine_marchal <antoine.marchal@obspm.fr>
2016-01-31 22:11:08 +0100

    fix

--- function.py
+++ function.py
@@ -77,6 +77,7 @@ def fact_SZE(nu) :
         import astropy.units as u
         from astropy.constants import G, h, k_B
         from astropy.cosmology import FLRW
-        x = u.h * nu / k_B / Tcmb0
+        from astropy import cosmology
+        x = u.h * nu / k_B / cosmology.Tcmb0
         f = x * ((ma.exp(x) + 1.) / (ma.exp(x) - 1.)) - 4.
         return f
