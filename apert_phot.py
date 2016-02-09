14f211510d029dbca92b3bd8e8039f3d3215219a
antoine marchal <amarchal@pccloned.ias.u-psud.fr>
2016-02-09 22:02:20 +0100

    add link to use photutils package

--- apert_phot.py
+++ apert_phot.py
@@ -1,12 +1,12 @@
+import sys
+sys.path.append('photutils')
 from photutils import aperture_photometry
-from astropy.utils.data import download_file
 import astropy.io.fits as pyfits
 import matplotlib.pyplot as plt
-from astropy import wcs
 import numpy as np
 import function as fct
 
-path ='patch_SZ/SZ/1_PSZ2 G000.13+78.04.fits'
+path ='patch_SZ/SZ/23_PSZ2 G006.76+30.45.fits'
 PSZ = "PSZ2v1.fits"
 NAME, GLON, GLAT = fct.coord_SZ(PSZ)
 map_data = pyfits.getdata(path)
